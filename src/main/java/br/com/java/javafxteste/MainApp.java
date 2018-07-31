fc363ef55e65e5cbed78914ee6d1a5c4847592ce
Henrique <henrique@datachamp.com.br>
2018-07-31 17:19:37 -0300

    alteracoes

--- src/main/java/br/com/java/javafxteste/MainApp.java
+++ src/main/java/br/com/java/javafxteste/MainApp.java
@@ -1,37 +1,104 @@
 package br.com.java.javafxteste;
 
+import br.com.java.controller.LoginController;
+import br.com.java.controller.inicialController;
+import br.com.java.model.User;
+import br.com.java.security.Authenticator;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 
 public class MainApp extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Inicial.fxml"));
-        
-        Scene scene = new Scene(root);
-        scene.getStylesheets().add("/styles/Styles.css");
-        
-        stage.setTitle("JavaFX and Maven");
-        stage.setScene(scene);
-        stage.show();
-    }
+    private Stage stage;
+    private User loggedUser;
+    private final double MINIMUM_WINDOW_WIDTH = 390.0;
+    private final double MINIMUM_WINDOW_HEIGHT = 500.0;
 
     /**
-     * The main() method is ignored in correctly deployed JavaFX application.
-     * main() serves only as fallback in case the application can not be
-     * launched through deployment artifacts, e.g., in IDEs with limited FX
-     * support. NetBeans ignores main().
-     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(MainApp.class, (java.lang.String[]) null);
     }
 
+//    @Override
+//    public void start(Stage primaryStage) {
+//        try {
+//            stage = primaryStage;
+//            stage.setTitle("FXML Login Sentus");
+//            stage.setMinWidth(MINIMUM_WINDOW_WIDTH);
+//            stage.setMinHeight(MINIMUM_WINDOW_HEIGHT);
+//            gotoLogin();
+//            primaryStage.show();
+//        } catch (Exception ex) {
+//            Logger.getLogger(MainApp.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+
+    public User getLoggedUser() {
+        return loggedUser;
+    }
+
+    public boolean userLogging(String userId, String password) {
+        if (Authenticator.validate(userId, password)) {
+            loggedUser = User.of(userId);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void userLogout() {
+        loggedUser = null;
+        gotoLogin();
+    }
+
+    private void gotoLogin() {
+        try {
+            LoginController login = (LoginController) replaceSceneContent("login.fxml");
+            login.setApp(this);
+        } catch (Exception ex) {
+            Logger.getLogger(MainApp.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private Initializable replaceSceneContent(String fxml) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        InputStream in = MainApp.class.getResourceAsStream(fxml);
+        loader.setBuilderFactory(new JavaFXBuilderFactory());
+        loader.setLocation(MainApp.class.getResource(fxml));
+        AnchorPane page;
+        try {
+            page = (AnchorPane) loader.load(in);
+        } finally {
+            in.close();
+        }
+        Scene scene = new Scene(page, 800, 600);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        return (Initializable) loader.getController();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Login.css");
+        
+        stage.setTitle("Sentus");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
